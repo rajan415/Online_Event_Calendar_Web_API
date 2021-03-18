d5c2b9fed7a527fbf2ac1cf01570eb2ea9e6cb9e
Ranjandip Singh <Rajansandhu37@gmail.com>
2021-03-19 12:57:26 +1300

    Add project files.

---
+++ DatabaseScripts/data.sql
@@ -0,0 +1,6 @@
+SET IDENTITY_INSERT [dbo].[Event] ON
+INSERT INTO [dbo].[Event] ([Id], [EventName], [StartDate], [EndDate]) VALUES (2, N'Auckland Book Fair', N'2021-03-20 09:00:00', N'2021-03-30 17:00:00')
+INSERT INTO [dbo].[Event] ([Id], [EventName], [StartDate], [EndDate]) VALUES (3, N'ABC Music Festival', N'2021-04-09 19:00:00', N'2021-04-13 22:00:00')
+INSERT INTO [dbo].[Event] ([Id], [EventName], [StartDate], [EndDate]) VALUES (4, N'Techno 2021 Exhibition ', N'2021-04-15 09:00:00', N'2021-05-05 22:00:00')
+INSERT INTO [dbo].[Event] ([Id], [EventName], [StartDate], [EndDate]) VALUES (5, N'Education For All 2021 : Seminarslllll', N'2021-05-26 09:00:00', N'2021-06-22 22:00:00')
+SET IDENTITY_INSERT [dbo].[Event] OFF
